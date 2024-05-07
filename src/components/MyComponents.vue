e9dff4e5e38c003ce248a44f74292cf6d52ed819
Анна <49556500+pin0k@users.noreply.github.com>
2024-05-07 23:23:36 +0300

    initial

---
+++ src/components/MyComponents.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="root">
+        <h1>{{ text }}</h1>
+        <label>
+            Введите e-mail:
+            <input 
+                type="email" 
+                v-bind:value="email"
+                v-on:input="emailInput($event.target.value)"
+            >
+        </label>
+        <label>
+            Введите пароль:
+            <input 
+                type="password" 
+                :value="password"
+                @input="passwordInput"
+            >
+        </label>
+    </div>
+    email: {{ email }}
+    password: {{ password }}
+</template>
+
+<script>
+export default {
+    name: 'MyComponent',
+    data() {
+        return {
+            text: 'Hello from 07.05.2024',  
+            email: 'example@mail.ru',
+            password: '12345678'
+        }
+    },
+    methods: {
+        emailInput(value) {
+            this.email = value  
+        },
+        passwordInput(evt) {
+            this.password = evt.target.value
+        }
+    }
+}
+</script>
