dc1e2ee58aa4f15e2bc847ee4fad1be86c077beb
A.Abakumova <a.abakumova@citrus-soft.ru>
2024-05-17 17:01:00 +0300

    добавление фото

--- src/components/PhotoEditor.vue
+++ src/components/PhotoEditor.vue
@@ -1,6 +1,19 @@
 <template>
     <div class="root">
         <h1>{{ text }}</h1>
+        <div class="container">
+            <div class="flex">
+                <div class="img-wrapper">
+                    <img src="../assets/images/cat.jpg">
+                </div>
+                <div class="controls">
+                    <h2>Шаверма-кот</h2>
+                    <button @click="isCatVisible = !isCatVisible">
+                        Показать / спрятать
+                    </button>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -9,8 +22,52 @@ export default {
     name: 'PhotoEditor',
     data() {
         return {
-            text: 'Hello from 17.05.2024',      
+            text: 'Hello from 17.05.2024',
+            isCatVisible: true, 
         }
     }
 }
 </script>
+<style>
+.container {
+    margin-top: 40px;
+}
+.flex {
+    display: flex;
+}
+.controls {
+    margin-left: 20px;
+}
+.img-wrapper {
+    width: 640px;
+    height: 480px;
+    background-color: #cecece;
+}
+img {
+    transition: 0.2s ease;
+}
+.sepia {
+    filter: sepia(100%);
+}
+.border {
+    border: 5px dashed #464646
+}
+.small {
+    width: 400px;
+}
+button {
+    margin-right: 10px;
+}
+button:active {
+    background-color: #dbdbdb;
+}
+h2 {
+    margin-bottom: 10px;
+}
+.btn-group {
+    margin-bottom: 20px;
+}
+input[type="range"] {
+    display: block;
+}
+</style>
